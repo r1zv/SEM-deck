596b5fba7d20e9df5b02cfd442f9ec057f6a3388
r1zv <rizv06@gmail.com>
2019-01-21 17:11:29 -0500

    react init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('My SEM Deck of Cards Setup using react, babel, webpack');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'SEM - Deck of Cards';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
